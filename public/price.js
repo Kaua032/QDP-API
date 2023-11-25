361ee0472985ab3838afebc70832b5607d1d8738
Kauã Xavier <kauaxavier032@gmail.com>
2023-11-25 18:23:33 -0300

    feat: Update price calculator script and add event listeners to date inputs

--- public/price.js
+++ public/price.js
@@ -1,2 +1,17 @@
-const roomPrice = document.getElementById("price")
+const roomPricePerNight = document.getElementById("price");
+const totalPrice = document.getElementById("totalPrice");
+const totalDays = document.getElementById("totalDays");
 
+function calculator() {
+  const checkin = new Date(document.getElementById("checkin").value);
+  const checkout = new Date(document.getElementById("checkout").value);
+
+  if (checkin && checkout) {
+    const diff = checkout.getTime() - checkin.getTime();
+
+    const diffDays = diff / (1000 * 3600 * 24);
+
+    totalDays.textContent = `${diffDays}`;
+    totalPrice.textContent = `${roomPricePerNight.value * diffDays}`;
+  }
+}
